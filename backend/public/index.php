2d32455c94cc763d50bf57fd3d9f1c372696703d
TJ Draper <tj@buzzingpixel.com>
2023-03-16 10:23:52 -0500

    Set up backend-core for dev

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,4 +2,20 @@
 
 declare(strict_types=1);
 
-echo 'hello world';
+use MissionControlBackend\Boot;
+use MissionControlBackend\Http\HttpRoutesConfig;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+(new Boot())
+    ->start(devMode: true)
+    ->registerEvents()
+    ->buildContainer()
+    ->buildHttpApplication()
+    ->applyRoutes(new HttpRoutesConfig(
+        authServerUrl: '',
+        accountServerUrl: '',
+    ))
+    ->registerErrorHandling(devMode: false)
+    ->registerHttpMiddleware()
+    ->emitResponse();
