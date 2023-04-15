c89ea8c8078334dde510835d8fa7ff1660327553
TJ Draper <tj@buzzingpixel.com>
2023-04-15 18:30:09 -0500

    Update build

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        './assets/**/*.{js,ts,jsx,tsx}',
+        './buzzingpixel-mission-control-frontend-core/assets/**/*.{js,ts,jsx,tsx}',
+    ],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+};
