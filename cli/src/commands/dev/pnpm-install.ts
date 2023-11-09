a2ac45fc6419c751d5d52b4421589ffb37020937
TJ Draper <tj@buzzingpixel.com>
2023-11-09 12:37:42 -0600

    Add pnpm cli commands

--- cli/src/commands/dev/pnpm-install.ts
+++ cli/src/commands/dev/pnpm-install.ts
@@ -1,7 +1,6 @@
 import { Command } from '@oclif/core';
 import { execSync } from 'node:child_process';
 import * as fs from 'fs-extra';
-import RemoveLinkedNodeModules from './remove-linked-node-modules';
 
 export default class PnpmInstall extends Command {
     // eslint-disable-next-line class-methods-use-this
@@ -12,12 +11,5 @@ export default class PnpmInstall extends Command {
             cd ${rootDir}/frontend;
             pnpm install;
         `, { stdio: 'inherit' });
-
-        const RemoveLinkedNodeModulesInstance = new RemoveLinkedNodeModules(
-            this.argv,
-            this.config,
-        );
-
-        await RemoveLinkedNodeModulesInstance.run();
     }
 }
--- cli/src/commands/dev/pnpm-upgrade-buzzing-pixel-packages.ts
+++ cli/src/commands/dev/pnpm-upgrade-buzzing-pixel-packages.ts
@@ -1,7 +1,6 @@
 import { Command } from '@oclif/core';
 import { execSync } from 'node:child_process';
 import * as fs from 'fs-extra';
-import RemoveLinkedNodeModules from './remove-linked-node-modules';
 
 export default class PnpmUpgradeBuzzingPixelPackages extends Command {
     // eslint-disable-next-line class-methods-use-this
@@ -18,12 +17,5 @@ export default class PnpmUpgradeBuzzingPixelPackages extends Command {
                 buzzingpixel-mission-control-url-monitoring \
              ;
         `, { stdio: 'inherit' });
-
-        const RemoveLinkedNodeModulesInstance = new RemoveLinkedNodeModules(
-            this.argv,
-            this.config,
-        );
-
-        await RemoveLinkedNodeModulesInstance.run();
     }
 }
